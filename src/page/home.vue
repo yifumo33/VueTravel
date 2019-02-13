c467c434ca8341f1d9c2a7ac48aec33069583c97
yifumo33 <yfumo@sina.com>
2019-02-13 11:16:21 +0800

    xxx

--- src/page/home.vue
+++ src/page/home.vue
@@ -3,6 +3,8 @@
 		<Header></Header>
 		<home-swiper></home-swiper>
 		<home-icons></home-icons>
+		<home-recommend></home-recommend>
+		<home-weekend></home-weekend>
 		hello world	
 
 	</div>
@@ -12,12 +14,16 @@
 	import Header from './components/Header.vue'
 	import HomeSwiper from './components/Swiper.vue'
 	import HomeIcons from './components/Icons.vue'
+	import HomeRecommend from './components/Recommend.vue'
+	import HomeWeekend from './components/Weekend.vue'	
 	export default{
 	name:"Home",
 	components:{
 			Header,
 			HomeSwiper,
-			HomeIcons
+			HomeIcons,
+			HomeRecommend,
+			HomeWeekend
 		}
 	}
 </script>
