d238f65d4716d08e40134c78675ae7d757d86e13
yifumo33 <yfumo@sina.com>
2019-02-14 09:27:27 +0800

    city

--- src/page/City.vue
+++ src/page/City.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
 		<city-header></city-header>
+		<city-search></city-search>
 	</div>
 </template>
 
 
 <script>
+	import CitySearch from './city-components/Search'
 	import CityHeader from './city-components/CityHeader'
 	export default{
 		 name:"City",
 		 components:{
-		 	CityHeader
+		 	CityHeader,
+		 	CitySearch
 		 }
 	}
 
---
+++ src/page/city-components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="search">
+		<input class="searchInput" type="text" placeholder="输入城市名或拼音">
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"CitySearch"
+	}
+</script>
+
+
+<style lang="stylus" scoped="">
+	@import '~style/varibles.styl'
+		.search
+			padding:.2rem
+			background:$bgColor
+			display:flex
+			justify-content:center
+			align-items:center
+			.searchInput
+				width:100%
+				height:.62rem
+				lien-height:.62rem
+				text-align:center
+				border-radius:.06rem
+				border:1px solid #ccc
+				box-sizing:border-box
+				padding:0 .2rem
+			color:#666
+</style>
