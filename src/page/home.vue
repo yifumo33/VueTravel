7811c63cbdf0a0517b65786b59f011ad13c134b7
yifumo33 <yfumo@sina.com>
2019-02-20 16:41:18 +0800

    xxsdfa

--- src/page/home.vue
+++ src/page/home.vue
@@ -28,7 +28,7 @@
 		},
 		methods:{
 			getHomeInfo(){
-				axios.get('VueTravel/dist/index.json').then(this.getHomeInfoSucc)
+				axios.get('dist/index.json').then(this.getHomeInfoSucc)
 			},
 			getHomeInfoSucc(res){
 				this.city = res.data.city
