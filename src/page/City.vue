ac1da13f50747fc3afab288375afc533e198359d
yifumo33 <yfumo@sina.com>
2019-02-20 15:30:49 +0800

    第四次提交

--- src/page/City.vue
+++ src/page/City.vue
@@ -31,7 +31,7 @@
 		 },
 		 methods:{
 		 	getCityInfo(){
-		 		axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+		 		axios.get('../../static/mock/city.json').then(this.handleGetCityInfoSucc)
 		 	},
 		 	handleGetCityInfoSucc(res){
 		 		res = res.data
